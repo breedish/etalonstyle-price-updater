723ca81376aac225450b12af9c932e78764261d9
breedish <breedish@gmail.com>
2016-04-24 16:08:18 +0200

    Fix failing installer

--- src/main/java/com/breedish/etalonstyle/process/UpdateOptions.java
+++ src/main/java/com/breedish/etalonstyle/process/UpdateOptions.java
@@ -1,17 +1,27 @@
 package com.breedish.etalonstyle.process;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.lang.Integer.valueOf;
+import static java.util.Collections.emptyMap;
 
 /**
  * Update options.
@@ -29,7 +39,7 @@ public class UpdateOptions {
 
     @PostConstruct
     public void init() throws Exception {
-        priceTypes = Files.lines(Paths.get(PriceUpdaterService.class.getResource(config).toURI()), Charset.forName("windows-1251"))
+        priceTypes = new BufferedReader(new InputStreamReader(UpdateOptions.class.getResourceAsStream(config))).lines()
             .map(line -> {
                 String values[] = line.split(",");
                 return new PriceType(values[0], valueOf(values[1]), valueOf(values[2]), valueOf(values[3]));
